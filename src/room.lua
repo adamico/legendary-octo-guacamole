b4328658218de7d798c30a50a7d6c557bada9bfd
Andrea D'Amico <andrea.damico@gmail.com>
2025-12-22 11:16:41 +0100

    feat: room carving

--- src/room.lua
+++ src/room.lua
@@ -1,6 +1,7 @@
 local Room = Class("Room")
 
-function Room:initialize(x, y, w, h)
+function Room:initialize(x, y, w, h, wall_options)
+    self.tiles = {x = x, y = y, w = w, h = h}
     self.pixels = {
         x = x * GRID_SIZE,
         y = y * GRID_SIZE,
@@ -9,9 +10,38 @@ function Room:initialize(x, y, w, h)
     }
 
     self.floor_color = 5
+    self.wall_options = wall_options or {1, 2}
+    self:carve()
+end
+
+function Room:carve()
+    local map_w = flr(SCREEN_WIDTH / GRID_SIZE)
+    local map_h = flr(SCREEN_HEIGHT / GRID_SIZE)
+
+    -- Fill screen with walls
+    for ty = 0, map_h do
+        for tx = 0, map_w do
+            local sprite = self.wall_options[1]
+            if #self.wall_options > 1 and rnd() < 0.1 then
+                sprite = self.wall_options[flr(rnd(#self.wall_options - 1)) + 2]
+            end
+            mset(tx, ty, sprite)
+        end
+    end
+
+    -- Carve floor
+    for ty = self.tiles.y, self.tiles.y + self.tiles.h - 1 do
+        for tx = self.tiles.x, self.tiles.x + self.tiles.w - 1 do
+            mset(tx, ty, 0)
+        end
+    end
 end
 
 function Room:draw()
+    self:draw_floor()
+end
+
+function Room:draw_floor()
     local rx = self.pixels.x
     local ry = self.pixels.y
     local rx2 = self.pixels.x + self.pixels.w
--- src/room_manager.lua
+++ src/room_manager.lua
@@ -2,15 +2,14 @@ local Room = require("room")
 
 local RoomManager = {}
 
--- RoomManager State
 RoomManager.current_room = nil
 
-function RoomManager.create_room(x, y, w, h)
-   return Room:new(x, y, w, h)
+function RoomManager.create_room(x, y, w, h, wall_options)
+   return Room:new(x, y, w, h, wall_options)
 end
 
 function RoomManager.init()
-   RoomManager.current_room = RoomManager.create_room(7, 3, 12, 11)
+   RoomManager.current_room = RoomManager.create_room(7, 3, 12, 11, {1, 2})
 end
 
 function RoomManager.draw()
